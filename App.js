d903d1fb7d734f161702bda8210946311d9ccdb5
Vincent Simon <vincent@simon.vg>
2022-06-12 00:18:49 +0200

    + navbar and settings

--- App.js
+++ App.js
@@ -6,68 +6,119 @@
  * @flow strict-local
  */
 
-import React, { useState } from 'react';
-import type {Node} from 'react';
+ import React, { useState } from 'react';
+ import * as rssParser from 'react-native-rss-parser';
+ import Icon from 'react-native-ico';
+ import { NavigationContainer } from '@react-navigation/native';
+ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import * as rssParser from 'react-native-rss-parser';
+ import {
+   SafeAreaView,
+   StatusBar,
+   StyleSheet,
+   Pressable,
+   Text,
+   View,
+   FlatList
+ } from 'react-native';
+ 
+ var iconHeight = 26;
+ var iconWidth = 26;
 
-import {
-  SafeAreaView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  View,
-  FlatList
-} from 'react-native';
+ const Tab = createBottomTabNavigator();
 
-const App: () => Node = () => {
+ export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+ function HomeScreen() {
+
+  const feeds = ['https://www.thm.de/wi/studium/sie-studieren/aktuelles?format=feed&type=rss','https://www.thm.de/wi/studium/sie-studieren/aktuelles?format=feed&type=rss']
+
+  feeds.forEach(function(x) {
+    console.log(feeds.length);
+  });
+
   const [rssFeedData, setRssFeedData] = useState('');
-  fetch('http://www.nasa.gov/rss/dyn/breaking_news.rss')
-          .then((response) => response.text())
-          .then((responseData) => rssParser.parse(responseData))
-          .then((rss) => {
-              console.log(rss);
-              setRssFeedData(rss);
-    });
+  console.log("TEST1");
+   fetch('https://www.thm.de/wi/studium/sie-studieren/aktuelles?format=feed&type=rss')
+           .then((response) => response.text())
+           .then((responseData) => rssParser.parse(responseData))
+           .then((rss) => {
+               console.log(rss);
+               setRssFeedData(rss);
+     });
 
-    const item = ( {item} ) => (
-      <Text style = {styles.item}>
-        {item.title}
-        {"\n"}
-        {item.description}
-        {"\n"}
-      </Text>
-    )    
+  const item = ( {item} ) => (
+    <Text style = {styles.listItem}>
+      {item.title}
+      {"\n"}
+      {item.date}
+      {"\n"}
+    </Text>
+  )
 
   return (
-    <SafeAreaView>
-      <StatusBar/>
-      <View style={styles.container}>
-        <FlatList
-          keyExtractor={(item => item.id)}
-          data = { rssFeedData.items }
-          renderItem={ item }
-          style={ styles.feed }
-        />
-      </View>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <FlatList
+        keyExtractor={(item => item.id)}
+        data = { rssFeedData.items }
+        renderItem={ item }
+        style={ styles.feed }
+      />
+    </View>
   );
-};
+}
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#fff',
-  },
-  item: {
-    margin: 5,
-    padding: 10,
-    paddingBottom: 1,
-    fontSize: 14,
-    backgroundColor: '#fff',
-  },
-  feed: {
-    color: 'black',
-  },
-});
-
-export default App;
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+  </View>
+  );
+}
+ 
+ const styles = StyleSheet.create({
+   container: {
+     width: "100%",
+     padding: 16,
+     paddingTop: 5,
+   },
+   listItem: {
+     padding: 20,
+     marginBottom: 5,
+     marginTop: 5,
+     borderRadius: 2,
+   },
+ 
+   container_nav: {
+     flex: 1,
+     backgroundColor: '#3962FF',
+     alignItems: 'center',
+     justifyContent: 'center',
+   },
+ 
+   NavContainer: {
+     position: 'absolute',
+     alignItems: 'center',
+     bottom: 20,
+   },
+ 
+   NavBar: {
+     flexDirection: 'row',
+     backgroundColor: '#eee',
+     width: '90%',
+     justifyContent: 'space-evenly',
+     borderRadius: 50,
+   },
+   Iconbehave: {
+     padding: 14,
+   }
+ });
