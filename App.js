82abd68a4a1a3b306df3292620eb47f9853caff8
kevin.biehl <kevin@biehl.dev>
2022-06-12 23:02:08 +0200

    Finish

--- App.js
+++ App.js
@@ -1,27 +1,19 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * - Klick auf Item öffnet Link / Info
- * - Herz-Button schreibt item in array fav / löschen / adden
- * - Settings Array mit feeds / löschen / adden
- * - Feeds combinieren
- * - *VLLT* Sharen Daten persistieren
- * - *VLLT* Sharen
- *
- *
- *
- *
- *
- * @format
- * @flow strict-local
+ * -----------------------------------------
+ *                  Newsbox
+ * -----------------------------------------
+ * Newsreader for RSS Feeds.
+ * Team 04: Kevin Biehl, Vincent Simon
+ * -----------------------------------------
+ * The content you see is work in progress!
  */
+
 import React, {useState} from 'react';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import * as rssParser from 'react-native-rss-parser';
-import {FAB, TextInput} from 'react-native-paper';
+import {FAB} from 'react-native-paper';
 
 import {
   StyleSheet,
@@ -29,7 +21,6 @@ import {
   View,
   FlatList,
   TouchableWithoutFeedback,
-  Settings,
   Linking,
 } from 'react-native';
 
@@ -45,8 +36,7 @@ let dateOptions = {
   day: 'numeric',
 };
 
-//STATIC
-
+// Demo Data
 feeds.push({
   title: 'THM',
   url: 'https://www.thm.de/wi/studium/sie-studieren/aktuelles?format=feed&type=rss',
@@ -71,7 +61,6 @@ export default function App() {
             } else if (route.name === 'Settings') {
               iconName = 'cog';
             }
-            // You can return any component that you like here!
             return <Icon name={iconName} size={size} color={color} />;
           },
           tabBarActiveTintColor: '#80ba24',
@@ -99,9 +88,6 @@ export default function App() {
 
 function FeedScreen() {
   const [rssFeedData, setRssFeedData] = useState('');
-  //feeds.forEach(function(x) {
-  //    console.log(x);
-  //});
 
   fetch(
     'https://www.thm.de/wi/studium/sie-studieren/aktuelles?format=feed&type=rss',
@@ -216,7 +202,7 @@ function FavScreen() {
 }
 
 function SettingsScreen() {
-  const [text, setText] = React.useState('');
+  const [setText] = React.useState('');
 
   const item = ({item}) => (
     <View>
@@ -255,7 +241,7 @@ function SettingsScreen() {
       />
       <View>
         <FAB
-          style={styles.AddlButton}
+          style={styles.AddButton}
           icon="plus"
           small
           onPress={() => {
@@ -301,43 +287,15 @@ const styles = StyleSheet.create({
     shadowRadius: 2.22,
     elevation: 3,
   },
-
-  container_nav: {
-    flex: 1,
-    backgroundColor: '#3962FF',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  NavContainer: {
-    position: 'absolute',
-    alignItems: 'center',
-    bottom: 20,
-  },
-
-  NavBar: {
-    flexDirection: 'row',
-    backgroundColor: '#eee',
-    width: '90%',
-    justifyContent: 'space-evenly',
-    borderRadius: 50,
-  },
-  Iconbehave: {
-    padding: 14,
-  },
-  FavButton: {
-    backgroundColor: '#80ba24',
-    left: 295,
-  },
-  DelButton: {
-    backgroundColor: '#ff365e',
-    left: 295,
-  },
-  AddlButton: {
+  AddButton: {
     backgroundColor: '#03b1fc',
     marginLeft: 100,
     marginRight: 100,
     margin: 50,
+  },
+  FavButton: {
+    backgroundColor: '#80ba24',
+    left: 295,
   },
   FavButtonContainer: {
     flex: 1,
@@ -345,6 +303,10 @@ const styles = StyleSheet.create({
     alignItems: 'flex-end',
     justifyContent: 'flex-end',
     margin: 5,
+  },
+  DelButton: {
+    backgroundColor: '#ff365e',
+    left: 295,
   },
   DelButtonContainer: {
     flex: 1,
@@ -354,47 +316,3 @@ const styles = StyleSheet.create({
     margin: 5,
   },
 });
-
-/*
-<TouchableWithoutFeedback onPress = {() => console.log('Selected Item :',item.title)}>
-  <View>
-    <Text style = {styles.listItem}>
-      {item.title}
-      {"\n"}
-      {item.imageUrl}
-      {"\n"}
-      {item.published}
-      {"\n"}
-    </Text>
-  </View>
-</TouchableWithoutFeedback>
-
-const item = ( {item} ) => (
-      <View>
-        <TapGestureHandler
-        waitFor={doubleTapRef}
-          onActivated={() => {
-            console.log('SINGLE TAP')
-          }}
-        >
-          <TapGestureHandler
-            ref={doubleTapRef}
-            numberOfTaps={2}
-            onActivated={() => {
-              console.log('DOUBLE TAP')
-            }}
-          >
-            <Text style = {styles.listItem}>
-              {item.title}
-              {"\n"}
-              {item.imageUrl}
-              {"\n"}
-              {item.published}
-              {"\n"}
-            </Text>
-          </TapGestureHandler>
-        </TapGestureHandler>
-      </View>
-    )
-
- */
